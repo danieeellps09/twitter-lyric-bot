e309ddaf4eea782fdfe0aa7d101af781e66388d4
daniel lopes <daniel-lps@hotmail.com.br~>
2023-09-23 00:38:11 -0300

    implementando a funcao handler que chama a postarTweet

--- netlify/functions/cronPostTwitter.ts
+++ netlify/functions/cronPostTwitter.ts
@@ -1,12 +1,21 @@
-// import { postarTweet } from "..";
-
+import { postarTweet } from "../..";
 import { Handler,Context,HandlerEvent } from "@netlify/functions";
 
 exports.handler = async() => {
+  try {
+    await postarTweet();
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Tarefa executada', }),
+      body: JSON.stringify({ message: 'Função postarTweet chamada com sucesso.' }),
     };
+  } catch (error) {
+    console.error('Erro ao chamar a função postarTweet:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Erro interno do servidor.' }),
+    };
+  }
 };
 
 
